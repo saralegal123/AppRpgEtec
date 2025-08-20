87403850de0933e66e75ccb79a4634d619707397
Sara Marcely Andrade de Oliveira <saradomacario@gmail.com>
2025-08-19 21:55:52 -0300

    PAM - AppRpgEtec / desenvolvimento do app para o rpgapi + criação das pastas Usuarios em Services e ViewModels

--- ViewModels/Usuarios/UsuarioViewModel.cs
+++ ViewModels/Usuarios/UsuarioViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using AppRpgEtec.Models;
 using AppRpgEtec.Services.Usuarios;
 
 namespace AppRpgEtec.ViewModels.Usuarios
@@ -49,7 +50,21 @@ namespace AppRpgEtec.ViewModels.Usuarios
         {
             try
             {
+                Usuario u = new Usuario();
+                u.Username = login;
+                u.PassowordString = senha;  
 
+                Usuario uAutenticado = await uService.PostAutenticarUsuarioAsync(u);
+                
+                //! - garante que o token não seja null ou vazio antes de prosseguir. Caso contrário, nao vai rolar
+                if (!string.IsNullOrEmpty(uAutenticado.Token))
+                {
+
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("Informação", "Dados incorretos. (¬_¬ )", "Ok");
+                }
             }
             catch (Exception ex)
             {
