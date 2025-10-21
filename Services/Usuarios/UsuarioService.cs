f0c9fb639ffc91c8e95cd8ece91e40638d586a37
coletti <matheus.coletti08@gmail.com>
2025-10-21 20:25:59 -0300

    PAM - AppRpgEtec // conclusao do mapa feito certo agr

--- Services/Usuarios/UsuarioService.cs
+++ Services/Usuarios/UsuarioService.cs
@@ -7,16 +7,15 @@ using System.Threading.Tasks;
 using AppRpgEtec.Models;
 
 namespace AppRpgEtec.Services.Usuarios
-{ // define a classe UsuarioService que herda da classe Request
+{ 
     public class UsuarioService : Request
     {
-        // cria uma instância privada da classe Request para fazer chamadas HTTP
+    
         private readonly Request _request;
 
         // define a URL base da API 
         private const string _apiUrlBase = "https://rpgsaramarcely.azurewebsites.net/Usuarios";
         
-        // construtor da classe, inicializa o objeto _request
         public UsuarioService()
         {
             _request = new Request();
@@ -32,26 +31,20 @@ namespace AppRpgEtec.Services.Usuarios
         // método assíncrono para registrar um novo usuário
         public async Task<Usuario> PostRegistrarUsuarioAsync(Usuario u)
         {
-            // define o complemento da URL para o endpoint de registro
             string urlComplementar = "/Registrar";
 
-            // envia os dados do usuário para a API usando POST e espera um inteiro como retorno
            u.Id = await _request.PostReturnIntAsync(_apiUrlBase + urlComplementar, u, string.Empty);
 
-            // retorna o objeto usuário recebido como parâmetro
             return u;
         }
 
-        // método assíncrono para autenticar um usuário
         public async Task<Usuario> PostAutenticarUsuarioAsync(Usuario u)
         {
-            // define o complemento da URL para o endpoint de autenticação
+         
             string urlComplementar = "/Autenticar";
-
-            // envia os dados do usuário para a API usando POST e recebe um objeto Usuario como resposta
+         
             u = await _request.PostAsync(_apiUrlBase + urlComplementar, u, string.Empty);
-
-            // retorna o objeto usuário atualizado com os dados recebidos da API
+           
             return u;
         }
         public async Task<int> PutAtualizarLocalizacaoAsync(Usuario u)
@@ -60,7 +53,7 @@ namespace AppRpgEtec.Services.Usuarios
             var result = await _request.PutAsync(_apiUrlBase + urlComplementar, u, _token);
             return result;
         }
-        //using System.Collections.ObjectModel
+      
         public async Task<ObservableCollection<Usuario>> GetUsuariosAsync()
         {
             string urlComplementar = string.Format("{0}", "/GetAll");
