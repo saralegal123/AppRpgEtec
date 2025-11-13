62156861faa86c36cee82d14f8f0ac60c4d268b0
Sara Marcely Andrade de Oliveira <saramarcely06@gmail.com>
2025-11-12 22:18:55 -0300

    PAM - AppRpgEtec //continuação das imagens

--- Services/Usuarios/UsuarioService.cs
+++ Services/Usuarios/UsuarioService.cs
@@ -1,66 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AppRpgEtec.Models;
+using System.Collections.ObjectModel;
 
 namespace AppRpgEtec.Services.Usuarios
-{ 
+{
     public class UsuarioService : Request
     {
-    
         private readonly Request _request;
-
-        // define a URL base da API 
         private const string _apiUrlBase = "https://rpgsaramarcely.azurewebsites.net/Usuarios";
-        
+
         public UsuarioService()
         {
             _request = new Request();
         }
-
         private string _token;
-        public UsuarioService(string token)
+
+        public UsuarioService(string Token)
         {
             _request = new Request();
-            _token = token;
+            _token = Token;
         }
 
-        // método assíncrono para registrar um novo usuário
         public async Task<Usuario> PostRegistrarUsuarioAsync(Usuario u)
         {
             string urlComplementar = "/Registrar";
-
-           u.Id = await _request.PostReturnIntAsync(_apiUrlBase + urlComplementar, u, string.Empty);
+            u.Id = await _request.PostReturnIntAsync(_apiUrlBase + urlComplementar, u, string.Empty);
 
             return u;
         }
 
         public async Task<Usuario> PostAutenticarUsuarioAsync(Usuario u)
         {
-         
             string urlComplementar = "/Autenticar";
-         
             u = await _request.PostAsync(_apiUrlBase + urlComplementar, u, string.Empty);
-           
+
             return u;
         }
+
         public async Task<int> PutAtualizarLocalizacaoAsync(Usuario u)
         {
             string urlComplementar = "/AtualizarLocalizacao";
             var result = await _request.PutAsync(_apiUrlBase + urlComplementar, u, _token);
             return result;
         }
-      
+
         public async Task<ObservableCollection<Usuario>> GetUsuariosAsync()
         {
             string urlComplementar = string.Format("{0}", "/GetAll");
             ObservableCollection<Models.Usuario> listaUsuarios = await
-            _request.GetAsync<ObservableCollection<Models.Usuario>>(_apiUrlBase + urlComplementar,
-            _token);
+            _request.GetAsync<ObservableCollection<Models.Usuario>>(_apiUrlBase + urlComplementar, _token);
+
             return listaUsuarios;
+        }
+
+        public async Task<int> PutFotoUsuarioAsync(Usuario u)
+        {
+            string urlComplementar = "/AtualizarFoto";
+            var result = await _request.PutAsync(_apiUrlBase + urlComplementar, u, _token); return result;
+        }
+
+        public async Task<Usuario> GetUsuarioAsync(int usuarioId)
+        {
+            string urlComplementar = string.Format("/{0}", usuarioId);
+            var usuario = await
+            _request.GetAsync<Models.Usuario>(_apiUrlBase + urlComplementar, _token);
+            return usuario;
+
         }
     }
 }
---
+++ ViewModels/Usuarios/ImagemUsuarioViewModel.cs
@@ -0,0 +1,153 @@
+﻿using AppRpgEtec.Models;
+using AppRpgEtec.Services.Usuarios;
+using Azure.Storage.Blobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AppRpgEtec.ViewModels.Usuarios
+{
+    public class ImagemUsuarioViewModel : BaseViewModel
+    {
+        private UsuarioService uService;
+        private static string conexaoAzureStorage = "";
+        private static string container = "arquivos";
+
+        public ImagemUsuarioViewModel()
+        {
+            string token = Preferences.Get("UsuariToken", string.Empty);
+            uService = new UsuarioService(token);
+
+            FotografarCommand = new Command(Fotografar);
+            SalvarImagemCommand = new Command(SalvarImagemAzure);
+            AbrirImagemCommand = new Command(AbrirGaleria);
+        }
+
+        public ICommand FotografarCommand { get; }
+        public ICommand SalvarImagemCommand { get; }
+        public ICommand AbrirImagemCommand { get; }
+
+        private ImageSource fonteImagem;
+
+        public ImageSource FonteImagem
+        {
+            get { return fonteImagem; }
+            set
+            {
+                fonteImagem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private byte[] foto;
+
+        public byte[] Foto
+        {
+            get => foto;
+            set
+            {
+                foto = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public async void AbrirGaleria()
+        {
+            try
+            {
+                if (MediaPicker.Default.IsCaptureSupported)
+                {
+                    FileResult photo = await MediaPicker.Default.PickPhotoAsync();
+
+                    if (photo != null)
+                    {
+                        using (Stream sourceStream = await photo.OpenReadAsync())
+                        {
+                            using (MemoryStream ms = new MemoryStream())
+                            {
+                                await sourceStream.CopyToAsync(ms);
+
+                                Foto = ms.ToArray();
+
+                                FonteImagem = ImageSource.FromStream(() => new MemoryStream(ms.ToArray()));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Ops", ex.Message, "Detalhes: " + ex.InnerException, "OK");
+            }
+        }
+
+        public async void Fotografar()
+        {
+            try
+            {
+                //Verificação se o dispositivo suporta mídia como câmera e galeria.
+                if (MediaPicker.Default.IsCaptureSupported)
+                {
+                    //Chamada para a câmera do dispositivo. Fica aguardando usuário tirar foto.
+                    FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+
+                    if (photo != null)
+                    {
+                        using (Stream sourceStream = await photo.OpenReadAsync())//Leitura dos bytes da foto para Stream
+                        {
+                            using (MemoryStream ms = new MemoryStream())
+                            {
+                                await sourceStream.CopyToAsync(ms);//Conversão do Stream para MemoryStream (arquivo em memória)
+
+                                //Carregamento do array de bytes a partir da memória para a propriedade da ViewModel
+                                Foto = ms.ToArray();
+
+                                //Carregamento do controle que apresenta a imagem para a ViewModel
+                                FonteImagem = ImageSource.FromStream(() => new MemoryStream(ms.ToArray()));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Ops", ex.Message, "Detalhes: " + ex.InnerException, "OK");
+            }
+        }
+
+        public async void SalvarImagemAzure()
+        {
+            try
+            {
+                Usuario u = new Usuario();
+                u.Foto = foto;
+                u.Id = Preferences.Get("UsuarioId", 0);
+
+                string fileName = $"{u.Id}.jpg";
+
+                //define o BLOB no qual a imagem será armazenada
+                var blobClient = new BlobClient(conexaoAzureStorage, container, fileName);
+
+                if (blobClient.Exists())
+                    blobClient.Delete();
+
+                using (var stream = new MemoryStream(u.Foto))
+                {
+                    blobClient.Upload(stream);
+                }
+
+                await Application.Current.MainPage.DisplayAlert("Mensagem", "Dados salvos com sucesso!", "Ok");
+                await App.Current.MainPage.Navigation.PopAsync();
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+
+    }
+}
