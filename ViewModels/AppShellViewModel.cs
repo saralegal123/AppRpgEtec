42bb9a1052e47cf4d597159fdbaf4d8130669b7e
Sara Marcely Andrade de Oliveira <saramarcely06@gmail.com>
2025-11-12 23:24:09 -0300

    PAM - AppRpgEtec / finalização

---
+++ ViewModels/AppShellViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+
+namespace AppRpgEtec.ViewModels
+{
+    public class AppShellViewModel : BaseViewModel
+    {
+        private UsuarioService uService;
+        public AppShellViewModel()
+        {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            uService = new UsuarioService(token);
+
+            CarregarUsuarioAzure();
+        }
+
+        private byte[] foto;
+        public byte[] Foto
+        {
+            get => foto;
+            set
+            {
+                foto = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public async void CarregarUsuarioAzure()
+        {
+            try
+            {
+                int usuarioId = Preferences.Get("UsuarioId", 0);
+                string fileName = $"{usuarioId}.jpg";
+                var blobClient = new BlobClient(conexaoAzureStorage, container, fileName);
+
+                if (blobClient.Exists())
+                {
+                    Byte[] fileBytes;
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        blobClient.OpenRead().CopyTo(ms);
+                        fileBytes = ms.ToArray();
+                    }
+                    Foto = fileBytes;
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+    }
+}
