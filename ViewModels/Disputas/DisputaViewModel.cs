04b8d7f2f5f25b20088bd2f416d43d1ff82a2fd1
coletti <matheus.coletti08@gmail.com>
2025-11-18 20:59:03 -0300

    PAM - AppRpgEtec // barra de pesquisa de disputas

---
+++ ViewModels/Disputas/DisputaViewModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Models;
+using AppRpgEtec.Services.Personagens;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace AppRpgEtec.ViewModels.Disputas
+{
+    public class DisputaViewModel : BaseViewModel
+    {
+        private PersonagemService pService;
+        public ObservableCollection<Personagem> PersonagensEncontrados { get; set; }
+        public Personagem Atacante { get; set; } 
+        public Personagem Oponente { get; set; }
+        public DisputaViewModel()
+        {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            pService = new PersonagemService(token);
+
+            Atacante = new Personagem();    
+            Oponente = new Personagem();
+
+            PersonagensEncontrados = new ObservableCollection<Personagem>();
+
+            PesquisarPersonagensCommand = new Command<string>(async (string pesquisa) => { await PesquisarPersonagens(pesquisa); });
+        }
+
+        public ICommand PesquisarPersonagensCommand { get; set; }
+
+        public string DescricaoPersonagemAtacante
+        {
+            get => Atacante.Nome;
+        }
+        public string DescricaoPersonagemOponente
+        {
+            get => Oponente.Nome;
+        }
+
+        private Personagem personagemSelecionado;
+       
+        public Personagem PersonagemSelecionado
+        {
+            set
+            {
+                if (value != null)
+                {
+                    personagemSelecionado = value;
+                    SelecionarPersonagem(personagemSelecionado);
+                    OnPropertyChanged();
+                    PersonagensEncontrados.Clear();
+                }
+            }
+        }
+
+        private string textoBuscaDigitado = string.Empty;
+        public string TextoBuscaDigitado
+        {
+            get { return textoBuscaDigitado; }
+            set
+            {
+                if ((value != null && !string.IsNullOrEmpty(value) && value.Length > 0))
+                {
+                    textoBuscaDigitado = value;
+                    _ = PesquisarPersonagens(textoBuscaDigitado);
+                }
+                else
+                {
+                    PersonagensEncontrados.Clear();
+                }
+            }
+        }
+        public async Task PesquisarPersonagens(string textoPesquisaPersonagem)
+        {
+            try
+            {
+                PersonagensEncontrados = await pService.GetByNomeAproximadoAsync(textoPesquisaPersonagem);
+                OnPropertyChanged(nameof(PersonagensEncontrados));
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Ops", ex.Message + "Detalhes:" + ex.InnerException, "Ok");
+            }
+        }
+
+        public async void SelecionarPersonagem(Personagem p)
+        {
+            try
+            {
+                string tipoCombatente = await Application.Current.MainPage
+                    .DisplayActionSheet("Atacante ou Oponente?", "Cancelar", "", "Atacante", "Oponente");
+
+                if (tipoCombatente == "Atacante")
+                {
+                    Atacante = p;
+                    OnPropertyChanged(nameof(DescricaoPersonagemAtacante));
+                }
+                else if (tipoCombatente == "Oponente")
+                {
+                    Oponente = p;
+                    OnPropertyChanged(nameof(DescricaoPersonagemOponente));
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "OK");
+            }
+        }
+    }
+}
